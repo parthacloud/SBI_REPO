162893d686a866339a3f49563073c9d5d6267de7
parthacloud <parthasarathidwibedy3@gmail.com>
2023-03-09 00:55:00 +0530

    task 101 changes

--- Demo.java
+++ Demo.java
@@ -3,6 +3,15 @@ heollllll
 int=10 
   int =20
 
+<<<<<<< Updated upstream
   }
 
 }
+=======
+//task-101 changes
+m1(){
+
+ }
+
+}
+>>>>>>> Stashed changes
---
+++ Greet.java
@@ -0,0 +1,4 @@
+//task 120 changes 
+ int a=10
+int b=20
+int c=30
